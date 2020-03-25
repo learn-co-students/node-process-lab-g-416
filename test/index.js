a880304cb9d986e76c7e59e95623541cc252f9d7
yazseyit77 <yazgeldi.seyidov@gmail.com>
2020-03-25 11:55:28 -0700

    Done.

--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
 var expect = require('chai').expect
 var fs = require('fs')
 var path = require('path')
-var expectedNodeVersion = 'v5.6.0'
+var expectedNodeVersion = 'v13.5.0'
 var expectedPlatform = ['darwin', 'linux']
 var expectedEnv = 'qa'
 
 describe('node', function () {
-  it('must have version ' + expectedNodeVersion, function(done){
+  it('must have version ' + expectedNodeVersion, function (done) {
     var actualNodeVersion = require(path.join(__dirname, '../version'))()
     expect(actualNodeVersion).to.be.equal(expectedNodeVersion)
     done()
@@ -14,7 +14,7 @@ describe('node', function () {
 })
 
 describe('platform', function () {
-  it('must be ' + expectedPlatform, function(done){
+  it('must be ' + expectedPlatform, function (done) {
     var actualPlatform = require(path.join(__dirname, '../platform'))()
     expect(actualPlatform).to.be.oneOf(expectedPlatform)
     done()
@@ -23,7 +23,7 @@ describe('platform', function () {
 
 
 describe('env var NODE_ENV', function () {
-  it('must be ' + expectedEnv, function(done){
+  it('must be ' + expectedEnv, function (done) {
     var actualEnv = require(path.join(__dirname, '../envvar'))()
     expect(actualEnv).to.equal(expectedEnv)
     done()
